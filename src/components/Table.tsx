64a10507928ceab968fdfa160fbde9600e749fff
Abui <adjiem31@gmail.com>
2021-07-29 06:17:42 +0700

    feat: add table

---
+++ src/components/Table.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/jsx-key */
+import clsx from 'clsx';
+import React from 'react';
+import { ChevronDown, ChevronUp } from 'react-bootstrap-icons';
+import {
+  HeaderGroup,
+  Row,
+  TableInstance,
+  TableOptions,
+  TableState,
+  useGlobalFilter,
+  UseGlobalFiltersInstanceProps,
+  useSortBy,
+  UseSortByColumnProps,
+  useTable,
+} from 'react-table';
+
+type PropsColumn<
+  T extends Record<string, unknown> = Record<string, unknown>,
+  TSort extends Record<string, unknown> = Record<string, unknown>
+> = HeaderGroup<T> & Partial<UseSortByColumnProps<TSort>>;
+type PropsReturn = TableInstance<Record<string, unknown>> & UseGlobalFiltersInstanceProps<Record<string, unknown>>;
+
+type TableProps<T extends Record<string, unknown>> = TableOptions<T> & {
+  search: (arg: {
+    state: TableState<Record<string, unknown>>;
+    preGlobalFilteredRows: Row<Record<string, unknown>>[];
+    setGlobalFilter: (filterValue: unknown) => void;
+  }) => JSX.Element;
+};
+
+function Table<T extends UseGlobalFiltersInstanceProps<T>>({
+  columns,
+  data,
+  search,
+}: TableProps<Record<string, unknown>>): JSX.Element {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable({ columns, data }, useGlobalFilter, useSortBy) as PropsReturn;
+
+  const renderHead = (column: { isSorted?: boolean; isSortedDesc?: boolean }) => {
+    if (column.isSortedDesc) return <ChevronUp />;
+    if (column.isSorted) return <ChevronDown />;
+    return '';
+  };
+
+  return (
+    <>
+      <div className="w-full">{search({ state, preGlobalFilteredRows, setGlobalFilter })}</div>
+      <table {...getTableProps()} className="table-fixed w-full">
+        <thead className="border-b border-solid border-blue-600 mb-6">
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column: PropsColumn) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps?.())} className="py-6 px-4 text-left">
+                  <div className="flex">
+                    {column.render('Header')}
+                    <span style={{ marginLeft: 8 }}>{renderHead(column)}</span>
+                  </div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <div className="h-6" />
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, index) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} className={clsx(index % 2 === 0 ? 'bg-blueGray-100' : '', 'rounded-lg')}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} className="py-3 px-4">
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Table;
--- src/pages/employee/index.tsx
+++ src/pages/employee/index.tsx
@@ -4,8 +4,40 @@ import { PlusLg, Search } from 'react-bootstrap-icons';
 
 import { CardDashboard } from '@/components/Container';
 import { Button, TextField } from '@/components/Form';
+import Table from '@/components/Table';
 
 const Home: NextPage<unknown> = () => {
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    []
+  );
   return (
     <CardDashboard
       title="Daftar Karyawan"
@@ -17,7 +49,9 @@ const Home: NextPage<unknown> = () => {
           </Button>
         </div>
       }
-    />
+    >
+      <Table columns={columns} data={data} search={() => <div />} />
+    </CardDashboard>
   );
 };
 
