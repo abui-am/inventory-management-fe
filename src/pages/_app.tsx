5932280972d076fc4879456ffa3558013ca3019b
Abui <adjiem31@gmail.com>
2021-07-29 17:05:58 +0700

    feat: add employee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -48,6 +48,15 @@ MyApp.getInitialProps = async ({ ctx }: AppContextType) => {
     ctx.res?.end();
     return {};
   }
+
+  // TODO : THIS IS TEMPORARY,
+  // delete when index page readt
+  if (cookie['INVT-TOKEN'] && ctx.pathname === '/') {
+    ctx.res?.writeHead(302, { Location: '/employee' });
+    ctx.res?.end();
+    return {};
+  }
+
   if (cookie['INVT-TOKEN'] && ctx.pathname === '/login') {
     ctx.res?.writeHead(302, { Location: '/' });
     ctx.res?.end();
