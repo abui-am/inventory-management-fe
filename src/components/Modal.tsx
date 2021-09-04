5f61c3d31b34c3779f10a721cfbebe8060b19f64
Abui <adjiem31@gmail.com>
2021-09-04 21:07:59 +0700

    refactor: change overlay background

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,11 +10,14 @@ const Modal: React.FC<Props> = ({ children, ...props }) => {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
+      TwShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      boxShadow: 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
+      borderRadius: 16,
     },
   };
 
   return (
-    <ReactModal {...props} style={customStyles}>
+    <ReactModal {...props} style={customStyles} overlayClassName="modal-overlay">
       <div className="max-w-md">{children}</div>
     </ReactModal>
   );
