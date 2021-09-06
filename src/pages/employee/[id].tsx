f26f499656eab13ee53abce39cfabca1518c13cd
Abui <adjiem31@gmail.com>
2021-09-06 12:08:08 +0700

    refactor: use CreateEmployeeForm in page

--- src/pages/employee/[id].tsx
+++ src/pages/employee/[id].tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/Button';
 import { CardDashboard } from '@/components/Container';
 import Tabs from '@/components/Tabs';
 import { useFetchEmployeeById } from '@/hooks/query/useFetchEmployee';
-import { EmployeeDetailRes } from '@/typings/employee';
+import { Employee } from '@/typings/employee';
 import { makeServerEmployee } from '@/utils/server';
 
 const EmployeeDetails = () => {
@@ -15,15 +15,15 @@ const EmployeeDetails = () => {
   const { query = {} } = useRouter();
   const { data } = useFetchEmployeeById(query.id as string);
 
-  const { firstName, lastName, position, ...rest } = data?.data ?? {};
+  const { first_name, last_name, ...rest } = data?.data.employee ?? {};
 
   return (
     <CardDashboard>
       <div className="flex mb-7">
         <img className="w-60 h-60" alt="profile" src="https://randomuser.me/api/portraits/women/44.jpg" />
         <div className="mt-2 ml-10">
-          <h1 className="text-2xl font-bold mb-2">{`${firstName} ${lastName}`}</h1>
-          <span className="text-blue-600 font-bold mb-6">{position}</span>
+          <h1 className="text-2xl font-bold mb-2">{`${first_name} ${last_name}`}</h1>
+          <span className="text-blue-600 font-bold mb-6">position</span>
           <div className="flex mt-6">
             <Button variant="gray">Edit Profile</Button>
           </div>
@@ -32,20 +32,20 @@ const EmployeeDetails = () => {
       <div className="flex mb-6">
         <Tabs menus={['Tentang', 'Akun Dashboard']} onClickTab={setActive} activeIndex={activeTab} />
       </div>
-      <EmployeeInfo data={rest as EmployeeDetailRes} />
+      <EmployeeInfo data={rest as Omit<Employee, 'first_name' | 'last_name'>} />
     </CardDashboard>
   );
 };
 
-const EmployeeInfo = ({ data }: { data: Omit<EmployeeDetailRes, 'firstName' | 'lastName' | 'position'> }) => {
-  const { birthday, gender, email, handphoneNumber, address } = data;
+const EmployeeInfo = ({ data }: { data: Omit<Employee, 'first_name' | 'last_name'> }) => {
+  const { birth_date, gender, email, phone_number, addresses } = data;
   return (
     <div>
       <div className="flex mb-4">
         <div className="flex flex-shrink-0 font-bold" style={{ flexBasis: 200 }}>
           Tanggal Lahir :
         </div>
-        <div className="flex">{dayjs(birthday).format('DD MMMM YYYY')}</div>
+        <div className="flex">{dayjs(birth_date).format('DD MMMM YYYY')}</div>
       </div>
       <div className="flex mb-4">
         <div className="flex flex-shrink-0 font-bold" style={{ flexBasis: 200 }}>
@@ -63,13 +63,13 @@ const EmployeeInfo = ({ data }: { data: Omit<EmployeeDetailRes, 'firstName' | 'l
         <div className="flex flex-shrink-0 font-bold" style={{ flexBasis: 200 }}>
           Nomor Hp :
         </div>
-        <div className="flex">{handphoneNumber}</div>
+        <div className="flex">{phone_number}</div>
       </div>
       <div className="flex mb-4">
         <div className="flex flex-shrink-0 font-bold" style={{ flexBasis: 200 }}>
           Address :
         </div>
-        <div className="flex">{address}</div>
+        <div className="flex">{addresses.filter((val) => val.title === 'Alamat Rumah')[0].complete_address}</div>
       </div>
     </div>
   );
